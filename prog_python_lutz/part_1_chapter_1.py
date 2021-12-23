ec43d8a1d7f16c4c29ee44ef4becfb53d8ada314
setloger <setloger@gmail.com>
2021-12-23 23:52:58 +0300

    new code. last page 74

--- prog_python_lutz/part_1_chapter_1.py
+++ prog_python_lutz/part_1_chapter_1.py
@@ -49,5 +49,45 @@ if __name__ == '__main__':
     for key in db:
         print(key, '=>\n ', db[key])
         
+## page 72 past class
+class Person:
+    def __init__(self, name, age, pay=0, job=None):
+        self.name = name
+        self.age = age
+        self.pay = pay
+        self.job = job 
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith', 42, 30000, 'software')
+    sue = Person('Sue Jones', 45, 40000, 'harware')
+    print(bob.name, sue.name)
+
+print(bob.name.split([-1]))
+sue.pay *= 1.1
+print(sue.pay)
+## page 74 past class add logic
+class Person:
+    def __str__(self):
+        return '<%s => %s>' %(self.__class__.__name__, self.__name__)
+    def __init__(self, name, age, pay=0, job=None):
+        self.name = name
+        self.age = age
+        self.pay = pay
+        self.job = job 
+
+    def lastName(self):
+        return self.name.split()[-1]
+    def giveRaise(self, percent):
+        self.pay *= (1.0 + percent)        
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith', 42, 30000, 'software')
+    sue = Person('Sue Jones', 45, 40000, 'harware')
+    print(bob.name, sue.pay)
+
+print(bob.lastName())
+sue.giveRaise(.10)
+print(sue.pay)
+
 
 
