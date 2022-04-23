c46f8fe2120abe3b9e13908a5c19e41f03cfaad8
setloger <setloger@gmail.com>
2022-04-23 20:00:00 +0300

    оптимизированы выстрелы корабля, доработы пули

--- other_exercises/book_pythoncrashcourse/alien_game/game_functions.py
+++ other_exercises/book_pythoncrashcourse/alien_game/game_functions.py
@@ -12,9 +12,16 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        '''Создание новой пули и включение ее в группу bullets'''
+        fire_bullet(ai_settings, screen, ship, bullets)
+def fire_bullet(ai_settings, screen, ship, bullets):
+    '''Выпускает пулю, если максимум еще не достигнут'''
+    '''Создание новой пули и включение ее в группу bullets'''
+    if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
+
+
+
 
 
 def check_keyup_events(event, ship):
@@ -36,7 +43,6 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
             
 
-
 def update_screen(ai_settings, screen, ship, bullets):
     '''Обновляет изображения на экране и отображает новый экран'''
     #При каждом проходе цикла перересовывается экран
@@ -47,5 +53,15 @@ def update_screen(ai_settings, screen, ship, bullets):
     ship.blitme()       
     #Отображение последнего прорисованного экрана.
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    '''Обновновляет позиции пуль и уничтожает старые пули'''
+    #Обновление позиции пуль
+    bullets.update()
+        #Удаление пуль, вышедших за край экрана
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
 
 
